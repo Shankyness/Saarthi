3c7589df9571844c99722b0387459029b28c28a3
Shankyness <Shashankkumar0107@gmail.com>
2024-07-12 19:20:05 +0530

    route and pages created

--- Blogverse/src/App.jsx
+++ Blogverse/src/App.jsx
@@ -1,7 +1,24 @@
+
+
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import SignIn from './pages/SignIn';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+       <Header />
+      <Routes>
+      <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/signin' element={<SignIn/>} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
